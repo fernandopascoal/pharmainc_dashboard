53128a52c768d8bf7d0bc102bdab56be7c898855
Fernando Pascoal <fernandodevpascoal@gmail.com>
2021-09-09 23:24:11 -0300

    finalizando o estilo do Clients

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,5 +1,6 @@
 import React, {useContext, useEffect} from "react"
 import { AppContext } from "../contexts/Store"
+import "../components/styles/Clients.css"
 
 
 
@@ -8,7 +9,7 @@ export default function Clients() {
     const {clients, setClients} = useContext(AppContext)
 
     const loadClients = async function (){
-        const response = await fetch("https://randomuser.me/api/?page=1&results=1&seed=abc")
+        const response = await fetch("https://randomuser.me/api/?page=1&results=50&seed=abc")
         const result = await response.json();   
         setClients(result.results)
     }
@@ -28,12 +29,16 @@ export default function Clients() {
         const id = `${client.id.name}${client.id.value}${client.cell}`
     
        return (
-            <div key={id}>
-                <p>
-                    <span>{name} </span>
-                    <span>{client.gender} </span>
-                    <span>{nasc}</span>
-                </p>
+            <div className="d-flex justify-content-center" key={id}>
+                <div className="d-flex justify-content-center m-0" id="clientsContent">
+                    <p className="border border-secundary m-0 d-flex justify-content-center align-items-center" id="clientName">{name} </p>
+                    <p className="border border-secundary d-flex justify-content-center align-items-center" id="clientGender">{client.gender} </p>
+                    <p  className="border border-secundary d-flex justify-content-center align-items-center" id="clientBirth">{nasc}</p>
+                    <div className="border border-secundary d-flex align-items-center justify-content-center" id="buttonContainer" >
+                        <button className="btn btn-info" id="moreDatails">More details</button>
+                    </div>
+                    
+                </div>
             </div>
        )
     })
