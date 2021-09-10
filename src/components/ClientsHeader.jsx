16984c22c311d07bc845465c3a31d42f64fbd508
Fernando Pascoal <fernandodevpascoal@gmail.com>
2021-09-09 23:01:22 -0300

    finalizando o estilo do ClientsHeader

--- src/components/ClientsHeader.jsx
+++ src/components/ClientsHeader.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import sort from "../imgs/sort.png"
+import "../components/styles/ClientsHeader.css"
 
 export default function ClientsHeader() {
     return(
-        <div>
-            <div>
-                <p>Name</p>
-                
-                <img src={sort} alt="" />
+        <div className="d-flex justify-content-center mt-5">
+            <div className="d-flex justify-content-center" id="clientsheader">
+                <div className="d-flex justify-content-center align-items-center border border-secundary" id="name">
+                    <p className="mt-3 me-5">Name</p>      
+                    <img src={sort} alt="" />
+                </div>
+                <p className="border border-secundary d-flex justify-content-center align-items-center" id="gender">Gender</p>
+                <p className="border border-secundary d-flex justify-content-center align-items-center" id="birth">Birth</p>
+                <p className="border border-secundary d-flex justify-content-center align-items-center" id="action">Action</p>
             </div>
-            <p>Gender</p>
-            <p>Birth</p>
-            <p>Action</p>
         </div>
     )
 }
